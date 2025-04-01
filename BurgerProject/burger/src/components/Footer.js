6640a117fa920cd2c83aca2055ee8e83a220243f
Uğur <ugursay3211@gmail.com>
2025-04-01 15:25:31 +0300

    BurgerProject
    This is a basic website built using React. It includes a simple UI with multiple pages and a responsive design.

--- BurgerProject/burger/src/components/Footer.js
+++ BurgerProject/burger/src/components/Footer.js
@@ -1,16 +1,34 @@
-import React from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import XIcon from "@mui/icons-material/X";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
 import "../styles/Footer.css";
 
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <FacebookIcon />
-        <InstagramIcon />
-        <XIcon />
+        <a
+          href="https://www.facebook.com/burgeryiyelim"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon />
+        </a>
+        <a
+          href="https://www.instagram.com/burgeryiyelimm"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon />
+        </a>
+        <a
+          href="https://www.instagram.com/burgeryiyelimm"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <YouTubeIcon />
+        </a>
       </div>
       <p>Tüm hakları saklıdır | BurgerYiyelim</p>
     </div>
--- BurgerProject/burger/src/components/Navbar.js
+++ BurgerProject/burger/src/components/Navbar.js
@@ -7,7 +7,9 @@ export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="main">
-        <img src={BurgerLogo} alt="" />
+        <Link to="/">
+          <img src={BurgerLogo} alt="" />
+        </Link>
         <div className="mainLink">
           <Link to="/">Anasayfa</Link>
           <Link to="/menu">Menu</Link>
